f2ffbac189eb0e684c9ff861e15ba52c0bf9494c
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2024-02-01 12:43:24 -0300

    first commit

---
+++ simpletext/src/main/java/com/simpletext/SimpleText.kt
@@ -0,0 +1,26 @@
+package com.simpletext
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SimpleText(
+    title: String = "hello world",
+    color: Color = Color.Black,
+    fontSize: TextUnit = 20.sp,
+    marginLeft: Dp = 0.dp,
+    marginRight: Dp = 0.dp,
+    fontWeight: FontWeight = FontWeight.Normal
+){
+    Text(text = title, style = TextStyle(color = color, fontSize = fontSize, fontWeight = fontWeight),
+        modifier = Modifier.padding(start = marginLeft, end = marginRight))
+}
