0cf6d8b3702ac711ffdcc461dade2c282d5e3d4c
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2024-02-03 21:35:02 -0300

    defining a max length

--- app/src/main/java/com/simpletextcontent/MainActivity.kt
+++ app/src/main/java/com/simpletextcontent/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SimpleText( title = "LOrem lorem paulo pedro")
+                    SimpleText( title = "LOrem lorem paulo pedro",maxLength = 10)
                 }
             }
         }
