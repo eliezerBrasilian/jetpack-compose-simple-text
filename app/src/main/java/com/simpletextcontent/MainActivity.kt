83737fca8b8e0b7452354a28d0a35afa70d1ce4a
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2024-02-03 21:45:08 -0300

    defining a max length

--- app/src/main/java/com/simpletextcontent/MainActivity.kt
+++ app/src/main/java/com/simpletextcontent/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.simpletext.SimpleText
+
 
 import com.simpletextcontent.ui.theme.SimpleTextTheme
 
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SimpleText( title = "LOrem lorem paulo pedro",maxLength = 10)
+                   // SimpleText( title = "LOrem lorem paulo pedro",maxLength = 10)
                 }
             }
         }
